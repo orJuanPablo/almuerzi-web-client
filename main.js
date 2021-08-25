34398e6bf749cac80a3eb79d20817e81d22a6199
juanPocampo <juanp.ocampo@hotmail.com>
2021-08-25 14:48:04 -0300

    Sección 22

---
+++ main.js
@@ -0,0 +1,75 @@
+const toHTML = (s) =>
+{
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(s,'text/html')
+    return doc.body.firstChild
+}
+const renderItem = (item) =>
+{
+    const elemento = toHTML(`<li data-id=${item._id}>${item.name}</li>`)
+
+    elemento.addEventListener('click', () => 
+    {
+        const mealsList = document.getElementById("meals-list")
+        Array.from(mealsList.children).forEach(x => x.classList.remove('selected'))
+        elemento.classList.add('selected')
+        const mealsIdInput = document.getElementById("meals-id")
+        mealsIdInput.value = item._id
+    })
+
+    return elemento
+}
+const renderOrder = (order, meals) => 
+{
+    const meal = meals.find(meal => meal._id == order.meal_id)
+    const elemento = toHTML(`<li data-id=${order._id}>${meal.name} - ${order.user_id}</li>`)
+    return elemento
+}
+window.onload = () =>
+{
+    const ordenar = document.getElementById('order')
+    ordenar.onsubmit = (evt) =>
+    {
+        evt.preventDefault()
+        const mealId = document.getElementById('meals-id')
+        const mealIdValue= mealId.value
+        if(!mealIdValue)
+        {
+            alert('Debes seleccionar un plato de comida.')
+            return
+        }
+        const order = {
+            meal_id: mealIdValue,
+            user_id: 'chanchito triste',
+        }
+        console.log(order)
+        fetch('http://localhost:3000/api/orders/',
+        {
+            method : 'POST',
+            headers : {'Content-Type':'application/json'},
+            body : JSON.stringify(order),
+        }).then(x => console.log(x))
+
+    }
+    fetch('http://localhost:3000/api/meals/')
+    .then(response => response.json())
+    .then(data => 
+        {
+            const mealsList = document.getElementById("meals-list")
+            const submit = document.getElementById("submit")
+            const itemList = data.map(renderItem)
+            mealsList.removeChild(mealsList.firstElementChild)
+            itemList.forEach(elemento => mealsList.appendChild(elemento))
+            submit.removeAttribute('disabled')
+            fetch('http://localhost:3000/api/orders/')
+             .then(response => response.json())
+             .then(ordersData =>
+                {
+                    const ordersList = document.getElementById('orders-list')
+                    const listOrders = ordersData.map(orderData => renderOrder(orderData, data))
+                    ordersList.removeChild(ordersList.firstElementChild)
+                    listOrders.forEach(elemento => ordersList.appendChild(elemento))
+                })
+        })
+    
+}
