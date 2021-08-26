a5b7e672e129f9180b2e91266ad77d4b5f3128d1
juanPocampo <juanp.ocampo@hotmail.com>
2021-08-26 19:12:02 -0300

    FINAL

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let mealState = []
 let ruta = 'login' //login, register, orders
+let user = {}
 const toHTML = (s) =>
 {
     const parser = new DOMParser()
@@ -27,7 +28,7 @@ const renderOrder = (order, meals) =>
     const elemento = toHTML(`<li data-id=${order._id}>${meal.name} - ${order.user_id}</li>`)
     return elemento
 }
-const initForm = () =>  
+const initOrder = () =>  
 {
     const ordenar = document.getElementById('order')
     ordenar.onsubmit = (evt) =>
@@ -40,17 +41,18 @@ const initForm = () =>
         if(!mealIdValue)
         {
             alert('Debes seleccionar un plato de comida.')
+            submit.removeAttribute('disabled')
             return
         }
         const order = {
             meal_id: mealIdValue,
-            user_id: 'Mi',
+            user_id: user._id,
         }
-        console.log(order)
         fetch('http://localhost:3000/api/orders/',
         {
             method : 'POST',
-            headers : {'Content-Type':'application/json'},
+            headers : {'Content-Type':'application/json',
+                        authorization: localStorage.getItem('token'),},
             body : JSON.stringify(order),
         }).then(x => x.json())
           .then(respuesta => 
@@ -86,10 +88,67 @@ const initData = () =>
                 })
         })
 }
+const renderLogin = () =>
+{
+    const loginView = document.getElementById('login-view')
+    document.getElementById('app').innerHTML = loginView.innerHTML
+
+    const loginForm = document.getElementById('login-form')
+    loginForm.onsubmit = (evt) =>
+    {
+        evt.preventDefault()
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        fetch('http://localhost:3000/api/auth/login', 
+        {   
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({email, password})
+        }).then(x => x.json())
+          .then(respuesta =>
+            {
+                localStorage.setItem('token', respuesta.token)
+                ruta = 'orders'
+                return respuesta.token
+                //renderOrders()
+            })
+          .then(token => 
+            {
+                return fetch('http://localhost:3000/api/auth/me',
+                {
+                    method: 'GET',
+                    headers:{'Content-Type': 'application/json',
+                              authorization: token,},    
+                })
+            })
+          .then(promise => promise.json())
+          .then(fetchedUser => 
+            {
+                localStorage.setItem('user',JSON.stringify(fetchedUser))
+                user = fetchedUser
+                renderOrders()
+            }) 
+    }
+}
+const renderOrders = () => 
+{
+    const ordersView = document.getElementById('orders-view')
+    document.getElementById('app').innerHTML = ordersView.innerHTML
+
+    initOrder()
+    initData()
+}
+const renderApp = () => 
+{
+    const token = localStorage.getItem('token')
+    if(token)
+    {
+        user = JSON.parse(localStorage.getItem('user'))
+        return renderOrders();
+    }
+    renderLogin()
+}
 window.onload = () =>
 {
-
-    //initForm()
-    //initData()  
-    
+    renderApp()     
 }
